8dbd667546fa3160e9f718f74b0f2843ed7e09b1
Maxim <mkwtzt@gmail.com>
2019-09-24 16:19:17 +0400

    fix name variables

--- js/setup.js
+++ js/setup.js
@@ -6,10 +6,10 @@ userDialog.classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 var WIZARDS_COUNT = 4;
-var wizardName = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var wizardSurname = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var getRandomInt = function (max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -19,9 +19,9 @@ var createWizards = function (count) {
   var wizards = [];
   for (var i = 0; i < count; i++) {
     var wizardsObject = {};
-    wizardsObject.name = wizardName[getRandomInt(wizardName.length)] + ' ' + wizardSurname[getRandomInt(wizardSurname.length)];
-    wizardsObject.coatColor = coatColors[getRandomInt(coatColors.length)];
-    wizardsObject.eyesColor = eyesColors[getRandomInt(eyesColors.length)];
+    wizardsObject.name = WIZARD_NAMES[getRandomInt(WIZARD_NAMES.length)] + ' ' + WIZARD_SURNAMES[getRandomInt(WIZARD_SURNAMES.length)];
+    wizardsObject.coatColor = COAT_COLORS[getRandomInt(COAT_COLORS.length)];
+    wizardsObject.eyesColor = EYES_COLORS[getRandomInt(EYES_COLORS.length)];
     wizards[i] = wizardsObject;
   }
   return wizards;
